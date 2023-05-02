eda49fed789290e483138c54cb3d1b88f382d62f
ItIzYe <aderuben0@gmail.com>
2023-05-02 16:05:34 +0200

    Commented main.cpp
    Die Datei wurde durch kommentiert, falls es Fehler gibt, bitte forken oder Ruben sagen

--- 00Timer/main.cpp
+++ 00Timer/main.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <stdlib.h>
-#include <windows.h>
-#include <cstdlib>
-#include <ctime>
-#include <math.h>
+#include <iostream>                            //
+#include <stdlib.h>                            //
+#include <windows.h>                           //
+#include <cstdlib>                             // => IMPORTIERT NÖTIGE PACKAGES DIE FÜR DAS KORREKTE LAUFEN DES PROGRAMMS BENÖTIGT WERDEN
+#include <ctime>                               //
+#include <math.h>                              //
 using namespace std;
-double tstart;;
+double tstart;;                                // => ERSTELLT FLOAT
 int main()
 {
-    while (1){
-    tstart = clock();
-    cout << " Start:"<< tstart ;
+    while (1){                                 // => 1 ENTSPRICHT True, KANN DURCH return 0; ODER break; GEBROCHEN WERDEN (EINFACH return 0; BENUTZEN BITTE)
+    tstart = clock();                          // => CLOCK AUS DEM <ctime> PACKAGE
+    cout << " Start:"<< tstart ;               // => cout == cONSOLE out, EINGABE IST cin == cONSOLE inPUT 
         for (double q=0;q<90000000;q++);
-    tstart = (clock()-tstart)/CLOCKS_PER_SEC;
+    tstart = (clock()-tstart)/CLOCKS_PER_SEC;  // CLOCKS_PER_SEC AUS <ctime> PACKAGE, MUSS MAN NICHT VIEL ERKLÄREN
     cout << " Dauer:" << tstart <<endl;
     }
-    return 0;
+    return 0;                                  // => OBEN GENANNTER BREAK VON DER SCHLEIFE
 }
